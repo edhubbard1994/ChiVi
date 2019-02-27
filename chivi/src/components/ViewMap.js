9c0c616bfc9dbd783615a4b39180a94866d6b769
Edward Hubbard <edwardhubbard@Edwards-MacBook-Air.local>
2019-02-27 02:35:47 -0600

    fixed map and env stuff

--- chivi/src/components/ViewMap.js
+++ chivi/src/components/ViewMap.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import Map from './ViewMap';
 import ReactMapGL from 'react-map-gl';
-
+require('dotenv').config();
 
 class ViewMap extends React.Component{
    
     state = {
         viewport: {
-            width: 400,
-            height: 400,
-            latitude: 37.7577,
-            longitude: -122.4376,
-            zoom: 8
+            width: '50%',
+            height: '75%',
+            latitude: 41.8850,
+            longitude: -87.6198,
+            zoom: 10
         }
     };
 
     render(){
-
+        const key = process.env.REACT_APP_MAPBOXAPIKEY;
+        console.log("key"+ key);
         const style  = {
             position: 'absolute',
-            top: 0,
+            textAlign: 'left',
+            top: '10%',
+            left: '25%',
             bottom: 0,
-            width: '200%'
+            width: '20%',
+            opacity: 0.9
           };
+
       
-        return(
-            <div >
-                <ReactMapGL style = {style}  {...this.state.viewport} onViewportChange={(viewport) => this.setState({viewport})}
-             mapboxApiAccessToken = 'pk.eyJ1IjoiZWRodWJiYXJkMTk5NCIsImEiOiJjamJmb3p2ZHQyeGNyMzJwZWFqeWd4ZDN6In0.yWQQD8j1AotGQkAeoQm7FA'
-             mapStyle = 'mapbox://styles/mapbox/streets-v11' />
-            </div>
-            
-        );
+        return(          
+            <ReactMapGL {...this.state.viewport} onViewportChange={(viewport) => this.setState({viewport})}
+            mapboxApiAccessToken = {key}
+             mapStyle = 'mapbox://styles/mapbox/dark-v9'
+             style = { style }  /> 
+            );
     }
 }
 
