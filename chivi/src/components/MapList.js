848ad3c58dc1987f04c2cc0703ec91598b5b240f
Edward Hubbard <edwardhubbard@Edwards-MacBook-Air.local>
2019-02-28 15:49:51 -0600

    popup for the map

---
+++ chivi/src/components/MapList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactMapGL from 'react-map-gl';
+import './comp.css';
+
+
+class MapList extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
