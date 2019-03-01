2aa610e4084c6bd897a8f3918cd7b6eed1dfa2d4
Edward <edhubbard1994@gmail.com>
2019-03-01 08:43:55 -0600

    Action Button

--- chivi/src/components/MapList.js
+++ chivi/src/components/MapList.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
 import ReactMapGL from 'react-map-gl';
 import './comp.css';
 
 
 class MapList extends React.Component{
+
     constructor(props){
         super(props);
         this.props.markers = props.markers;
+        this.state.show = false;
+    }  
+
+    show(){
+        this.setState({show : true});
     }
+
+    hide(){
+        this.setState({show : false});
+    }
+
     render(){
         return(
             <div>
-                <ul>{this.props.markers}</ul>
+                <Popup trigger = {this.state.show} 
+                modal
+                closeOnDocumentClick>
+                    <ul>{this.props.markers}</ul>
+                </Popup> 
             </div>
         );
     }
 }
+
+export default MapList;
--- chivi/src/components/ViewMap.js
+++ chivi/src/components/ViewMap.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactMapGL from 'react-map-gl';
 import './comp.css';
+import MapList from './MapList';
 require('dotenv').config();
 
 class ViewMap extends React.Component{
@@ -23,6 +24,8 @@ class ViewMap extends React.Component{
         this.props = props;
     }
 
+   
+
     render(){
         const key = process.env.REACT_APP_MAPBOXAPIKEY;
         console.log("key"+ key);
@@ -40,10 +43,11 @@ class ViewMap extends React.Component{
         return(          
             <div>
                 <h1 style = {{color: 'white', opacity: 0.9}} >Visualize</h1>
-                <ReactMapGL {...this.state.viewport} onViewportChange={(viewport) => this.setState({viewport})}
+                <ReactMapGL  {...this.state.viewport} onViewportChange={(viewport) => this.setState({viewport})}
                     mapboxApiAccessToken = {key}
                      mapStyle = 'mapbox://styles/mapbox/dark-v9'
-                    style = { style }  /> 
+                    style = { style }></ReactMapGL>
+                     <button className = 'popUpBtn'>Action</button>                            
             </div>
             );
     }
