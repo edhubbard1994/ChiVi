2eb6020489b769993190aa0407fe1340a5876068
Edward Hubbard <edhubbard1994@gmail.com>
2019-03-27 02:33:48 -0500

    to show commits

--- chivi/src/components/MapListItem.js
+++ chivi/src/components/MapListItem.js
@@ -2,20 +2,16 @@ import React from 'react';
 import ReactMapGL from 'react-map-gl';
 import './comp.css';
 
-
+//TODO: edit make this fit the paradigm
 class MapListItem extends React.Component{
     constructor(props){
         super(props);
-        if(this.props.pin)//pin will be the neccessary info from a map marker object
-            this.props.pin = props.pin;
+        
     }
 
     render(){
         return( 
-            <div>
-                <h2>{this.props.pin.name}</h2>
-                <h3>{this.props.pin.head}</h3>
-                <p>{this.props.pin.post}</p>
+            <div className = 'ListItem'>
             </div>
         );
 
