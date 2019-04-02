c7d8228ca95d17900e669af8262221036ac01c64
Edward Hubbard <edhubbard1994@gmail.com>
2019-04-02 16:18:43 -0500

    stubs for local data

--- server/server.py
+++ server/server.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'home'
 
+@app.route('/localdata', methods = ['GET','POST','PUT'])
+def local_data():
+    if request.method == 'GET':
+        ##do Get request here
+        return
+    
+    elif request.method == 'POST':
+        ##do Post request stuff here
+        return
+
+    else:
+        ##do put request stuff here
+        return
 
